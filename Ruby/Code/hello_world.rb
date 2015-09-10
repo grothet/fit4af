421deae2b9de017bdb6f496bf6ae8ff75091553f
grothet <thomas.grotheer@gmx.de>
2015-09-10 18:41:01 +0200

    Ruby Basis

---
+++ Ruby/Code/hello_world.rb
@@ -0,0 +1,8 @@
+	puts "Die \"Woche\" besteht aus:\n\tMontag\n\tDienstag."
+
+	var = "Hello Ruby"
+	var2 = "heute ist der 09.09.2015 + "
+	var3 = 1
+	puts var + " " + var2 + var3.to_s
+
+	puts "#{var} #{Time.new} #{var3}"
