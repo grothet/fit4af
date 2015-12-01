aa85b12334fc338d8bacb60d4b6903dd6590fac4
grothet <thomas.grotheer@gmx.de>
2015-12-01 19:42:34 +0100

    2015-12-01

--- Rails/Projekte/flix/app/controllers/movies_controller.rb
+++ Rails/Projekte/flix/app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
 	def index
-		@movies = Array.new
-		@movies = ["Iron Man", "Superman", "Spider Man"]
+		@movies = Movie.all
+		#@movies = Array.new
+		#@movies = ["Iron Man", "Superman", "Spider Man"]
 	end
 end
