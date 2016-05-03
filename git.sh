c0e78e4ba9888ada9ac494eb7f2d29c9938e954d
grothet <thomas.grotheer@gmx.de>
2016-05-03 20:52:46 +0200

    2016-05-03

---
+++ git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Git status wird geprüft"
+git status
+
+while [[ $auswahl != "2" ]]
+do
+	echo " 1: Daten werden in Github hochgeladen"
+	echo " 2: Exit"
+
+	read -p "Eingabe: " auswahl
+
+	if [[ $auswahl = "1" ]]
+	then
+		echo "Hochladen..."
+		git add . -A
+		read -p "Bezeichnung: " bezeichnung
+		git commit -am "$bezeichnung"
+	fi
+done
+echo "Exit"
