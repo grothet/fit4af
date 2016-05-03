1f54528f9989b72e178d85214090b45f4e47038c
grothet <thomas.grotheer@gmx.de>
2016-05-03 21:02:06 +0200

    2016-05-03

--- git.sh
+++ git.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "Git status wird geprüft"
-git status
-
-while [[ $auswahl != "2" ]]
+while [[ $auswahl != "3" ]]
 do
-	echo " 1: Daten werden in Github hochgeladen"
-	echo " 2: Exit"
+	echo " 1: Status Prüfen"
+	echo " 2: Daten werden in Github hochgeladen"
+	echo " 3: Exit"
 
 	read -p "Eingabe: " auswahl
-
+	
 	if [[ $auswahl = "1" ]]
+	then
+		echo "Git status wird geprüft"
+		git status
+	fi
+
+	if [[ $auswahl = "2" ]]
 	then
 		echo "Hochladen..."
 		git add . -A
