143a481b326fc2cca7e13f7c203d7379a2e5d915
grothet <thomas.grotheer@gmx.de>
2016-06-08 20:11:51 +0200

    2016-06-08,

--- git.sh
+++ git.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-while [[ $auswahl != "3" ]]
+while [[ $auswahl != "4" ]]
 do
 	Datum=$(date +%Y-%m-%d)
 
 	echo " 1: Status Prüfen"
 	echo " 2: Daten werden in Github hochgeladen"
-	echo " 3: Exit"
+	echo " 3: Daten werden aus Github geladen"
+	echo " 4: Exit"
 
 	read -p "Eingabe: " auswahl
 	
@@ -24,5 +25,12 @@ do
 		git commit -am "$Datum, $bezeichnung"
 		git push origin master
 	fi
+	
+	if [[ §auswahl = "3" ]]
+	then
+		echo "Runterladen..."
+		git pull origin master
+	fi
+
 done
 echo "Exit..."
